a961cb66d6ca0800cf7f90e398f20633aebdffe8
Patrick Wilshaw <patrick.wilshaw@johnlewis.co.uk>
2020-05-13 16:15:13 +0100

    function

--- story.py
+++ story.py
@@ -1,13 +1,18 @@
 from urllib.request import urlopen
 
-story = urlopen("http://sixty-north.com/c/t.txt")
-story_words=[]
-for line in story:
-    #   print("JIRA-200xx")
-    line_words = line.decode('utf-8').split()
-    for word in line_words:
-        story_words.append(word)
-story.close()
+def fetch_words():
+    story = urlopen("http://sixty-north.com/c/t.txt")
+    story_words=[]
+    for line in story:
+        #   print("JIRA-200xx")
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
 
-for word in story_words:
-    print(word)
+    for word in story_words:
+        print(word)
+
+print(__name__)
+if __name__ == "__main__":
+    fetch_words()
