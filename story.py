b4aeb09186bf50b869d7a7e571f2aed3b43f416f
Patrick Wilshaw <patrick.wilshaw@johnlewis.co.uk>
2020-05-13 16:31:19 +0100

    main

--- story.py
+++ story.py
@@ -1,7 +1,9 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    story = urlopen("http://sixty-north.com/c/t.txt")
+def fetch_words(url):
+    #"http://sixty-north.com/c/t.txt"
+    story = urlopen(url)
     story_words=[]
     for line in story:
         #   print("JIRA-200xx")
@@ -9,10 +11,16 @@ def fetch_words():
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
-    for word in story_words:
+def print_items(items):
+    for word in items:
         print(word)
 
-print(__name__)
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
+#print(__name__)
 if __name__ == "__main__":
-    fetch_words()
+   main(sys.argv[1])
